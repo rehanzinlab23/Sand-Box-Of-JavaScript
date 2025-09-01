3e0d1186620866cf2b2ed70db0baf24394f4852c
rehanzinlab23 <rehan.zinlab@gmail.com>
2025-09-01 11:37:15 +0500

    functions

--- JavaScript Sandbox/Arrays and Objects/Arrays Challenge/script.js
+++
@@ -1,9 +0,0 @@
-const arr = [1, 2, 3, 4, 5];
-
-arr.reverse();
-
-arr.push(0);
-
-arr.unshift(6);
-
-console.log(arr);
--- JavaScript Sandbox/Variables/Capitalize Callenge/script.js
+++
@@ -1,4 +0,0 @@
-const myString = 'developer';
-let myNewString;
-myNewString = myString[0].toUpperCase() + myString.substring(1);
-console.log(myNewString);
