f7ee8ee0c1b9224ffb00b4b9c55d12d4bad719b5
rehanzinlab23 <rehan.zinlab@gmail.com>
2025-09-01 13:30:35 +0500

    arrow

---
+++ Functions/Deceleration vs Expression/script.js
@@ -0,0 +1,15 @@
+// Function Deceleration
+
+function dollar(value) {
+     return '$' + value;
+}
+
+console.log(dollar(100));
+
+// Function Expression
+
+const plus = function (value) {
+        return '+' + value;
+};
+
+console.log(plus(200));
