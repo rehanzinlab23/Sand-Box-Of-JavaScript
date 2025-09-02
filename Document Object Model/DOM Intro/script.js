3b5da0d6c4adc61bcd8080065b2c37b0057eb93f
rehanzinlab23 <rehan.zinlab@gmail.com>
2025-09-02 16:49:36 +0500

    DOM

---
+++ Document Object Model/DOM Intro/script.js
@@ -0,0 +1,23 @@
+// console.log(window);
+
+// console.log(document);
+
+// console.log(document.body.innerHTML);
+
+// console.log(document.links[0].hostname);
+
+// console.log(document.links[2]);
+
+// document.body.innerHTML = '<h1>Hello World!</h1>'
+
+// Not Very Helpfull and not even uses very much
+// document.write('Hello from Spidey');
+
+// Very Helpfull and used very much
+
+const main = document.querySelector('#main');
+console.log(main);
+
+// main.innerHTML = '<h1>Hello World!</h1>';
+
+document.querySelector('#main h1').innerHTML = 'Hello';
