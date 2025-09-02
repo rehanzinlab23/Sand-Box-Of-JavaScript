932920317529ced228df2712a81bc7051b93c977
rehanzinlab23 <rehan.zinlab@gmail.com>
2025-09-02 11:39:18 +0500

    asdfds

---
+++ Iteration Array Methods/Break and Continue/script.js
@@ -0,0 +1,19 @@
+// Break
+
+// for (let i = 0; i <= 20; i++) {
+// if (i === 155) {
+//      console.log('Breaking...');
+//      break;
+// }
+// console.log(i);
+// }
+
+// Conitnue 
+
+for (let i = 0; i <= 20; i++) {
+if (i === 13) {
+     console.log('Skipping 13...');
+     continue;
+}
+console.log(i);
+}
